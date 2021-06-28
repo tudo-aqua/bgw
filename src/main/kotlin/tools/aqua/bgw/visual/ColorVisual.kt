1ac0574a6e7e86e67d4b2db1e06ce2f47e104c56
Dominik Mäckel <dominik.maeckel@tu-dortmund.de>
2021-06-28 11:13:58 +0200

    Added predefined colors in ColorVisual Closes #38

--- src/main/kotlin/tools/aqua/bgw/visual/ColorVisual.kt
+++ src/main/kotlin/tools/aqua/bgw/visual/ColorVisual.kt
@@ -41,4 +41,71 @@ class ColorVisual(color: Color) : SingleLayerVisual() {
 	 * @param a alpha channel. Default: 255.
 	 */
 	constructor(r: Int, g: Int, b: Int, a: Int = 255) : this(Color(r, g, b, a))
+	
+	companion object {
+		/**
+		 * ColorVisual filled WHITE.
+		 */
+		val WHITE: ColorVisual = ColorVisual(Color.WHITE)
+		
+		/**
+		 * ColorVisual filled LIGHT_GRAY.
+		 */
+		val LIGHT_GRAY: ColorVisual = ColorVisual(Color.LIGHT_GRAY)
+		
+		/**
+		 * ColorVisual filled GRAY.
+		 */
+		val GRAY: ColorVisual = ColorVisual(Color.GRAY)
+		
+		/**
+		 * ColorVisual filled DARK_GRAY.
+		 */
+		val DARK_GRAY: ColorVisual = ColorVisual(Color.DARK_GRAY)
+		
+		/**
+		 * ColorVisual filled BLACK.
+		 */
+		val BLACK: ColorVisual = ColorVisual(Color.BLACK)
+		
+		/**
+		 * ColorVisual filled RED.
+		 */
+		val RED: ColorVisual = ColorVisual(Color.RED)
+		
+		/**
+		 * ColorVisual filled PINK.
+		 */
+		val PINK: ColorVisual = ColorVisual(Color.PINK)
+		
+		/**
+		 * ColorVisual filled ORANGE.
+		 */
+		val ORANGE: ColorVisual = ColorVisual(Color.ORANGE)
+		
+		/**
+		 * ColorVisual filled YELLOW.
+		 */
+		val YELLOW: ColorVisual = ColorVisual(Color.YELLOW)
+		
+		/**
+		 * ColorVisual filled GREEN.
+		 */
+		val GREEN: ColorVisual = ColorVisual(Color.GREEN)
+		
+		/**
+		 * ColorVisual filled MAGENTA.
+		 */
+		val MAGENTA: ColorVisual = ColorVisual(Color.MAGENTA)
+		
+		/**
+		 * ColorVisual filled CYAN.
+		 */
+		val CYAN: ColorVisual = ColorVisual(Color.CYAN)
+		
+		/**
+		 * ColorVisual filled BLUE.
+		 */
+		val BLUE: ColorVisual = ColorVisual(Color.BLUE)
+	}
 }
