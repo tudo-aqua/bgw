c59fbd1218ea1d67028db01993a2ffac0a803ea8
Dominik Mäckel <dominik.maeckel@tu-dortmund.de>
2021-07-02 19:45:08 +0200

    Applied publishing code from z3-turnkey. #59

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,10 +8,11 @@ plugins {
 	id("io.gitlab.arturbosch.detekt") version "1.17.0"
 	id("maven-publish")
 	kotlin("jvm") version "1.5.0"
+	signing
 }
 
-group = "tools.aqua"
-version = "1.0"
+group = "io.github.tudo-aqua"
+version = "0.1-SNAPSHOT"
 
 repositories {
 	mavenCentral()
@@ -86,10 +87,75 @@ java {
 publishing {
 	publications {
 		create<MavenPublication>("maven") {
-			groupId = "tools.aqua"
+			groupId = "io.github.tudo-aqua"
 			artifactId = "bgw-core"
 			version = "0.1-SNAPSHOT"
 			from(components["java"])
+			
+			pom {
+				name.set("bgw-core")
+				
+				description.set(
+					"A framework for board game applications."
+				)
+				
+				url.set("https://github.com/tudo-aqua/bgw-core")
+				
+				licenses {
+					license {
+						name.set("The MIT License")
+						url.set("https://opensource.org/licenses/MIT")
+					}
+					license {
+						name.set("ISC License")
+						url.set("https://opensource.org/licenses/ISC")
+					}
+				}
+				
+				developers {
+					developer {
+						name.set("Stefan Naujokat")
+						email.set("stefan.naujokat@cs.tu-dortmund.de")
+					}
+					developer {
+						name.set("Till Schallau")
+						email.set("till.schallau@tu-dortmund.de")
+					}
+					developer {
+						name.set("Dominik Mäckel")
+						email.set("dominik.maeckel@cs.tu-dortmund.de")
+					}
+					developer {
+						name.set("Fabian Klümpers")
+						email.set("fabian.kluempers@cs.tu-dortmund.de")
+					}
+				}
+				
+				scm {
+					connection.set("scm:git:git://github.com:tudo-aqua/bgw-core.git")
+					developerConnection.set("scm:git:ssh://git@github.com:tudo-aqua/bgw-core.git")
+					url.set("https://github.com/tudo-aqua/bgw-core/tree/main")
+				}
+			}
 		}
 	}
+	repositories {
+		maven {
+			name = "nexusOSS"
+			val releasesUrl = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+			val snapshotsUrl = uri("https://oss.sonatype.org/content/repositories/snapshots/")
+			url = if (version.toString().endsWith("SNAPSHOT")) snapshotsUrl else releasesUrl
+			credentials {
+				username = properties["nexusUsername"] as? String
+				password = properties["nexusPassword"] as? String
+			}
+		}
+	}
+}
+
+
+signing {
+	isRequired = !hasProperty("skip-signing")
+	useGpgCmd()
+	sign(publishing.publications["maven"])
 }
