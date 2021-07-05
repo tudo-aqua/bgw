f2aa6a5007370de83d5161fd558e084e8795f9c3
Fabian Klümpers <fabian.kluempers@udo.edu>
2021-07-05 13:56:29 +0200

    Fix doc in BoardGameApplication.kt. Closes #79

--- src/main/kotlin/tools/aqua/bgw/core/BoardGameApplication.kt
+++ src/main/kotlin/tools/aqua/bgw/core/BoardGameApplication.kt
@@ -22,122 +22,123 @@ import java.util.*
  * [Scene]s get shown by calling [showMenuScene] and [showGameScene].
  * Application starts by calling [show].
  *
- * @param windowTitle Window title displayed in the title bar.
+ * @param windowTitle title for the application window. Gets displayed in the title bar.
+ *      Default: "BoardGameWork Application".
  *
  * @see BoardGameScene
  * @see MenuScene
  */
 open class BoardGameApplication(windowTitle: String = "BoardGameWork Application") {
-	
-	/**
-	 * [Frontend] instance.
-	 */
-	private val frontend: Frontend = Frontend()
-	
-	/**
-	 * Window title displayed in the title bar.
-	 */
-	var title: String
-		get() = Frontend.titleProperty.value
-		set(value) {
-			Frontend.titleProperty.value = value
-		}
-	
-	/**
-	 * Background [Visual] for the [BoardGameApplication]. [Visual] appears as bars window does not match [Scene] ratio.
-	 * Do not mix up this [Property] with the [Scene] background [Visual] [Scene.background].
-	 */
-	var background: Visual
-		get() = Frontend.backgroundProperty.value
-		set(value) {
-			Frontend.backgroundProperty.value = value
-		}
-	
-	init {
-		title = windowTitle
-	}
-	
-	/**
-	 * Shows the given [FileDialog].
-	 *
-	 * @param dialog the [FileDialog] to be shown.
-	 *
-	 * @return chosen file(s) or [Optional.empty] if canceled.
-	 */
-	fun showFileDialog(dialog: FileDialog): Optional<List<File>> = Frontend.showFileDialog(dialog)
-	
-	/**
-	 * Shows a dialog containing the given [message] and [buttons].
-	 *
-	 * @param alertType the [AlertType] of the alert. Affects the displayed icon.
-	 * @param message message to be shown.
-	 * @param buttons buttons to be shown.
-	 *
-	 * @return chosen button or [Optional.empty] if canceled.
-	 */
-	fun showAlertDialog(alertType: AlertType, message: String, vararg buttons: ButtonType): Optional<ButtonType> =
-		Alert(
-			alertType.toAlertType(),
-			message,
-			*buttons.map { it.toFXButtonType() }.toTypedArray()
-		).showAndWait().map { it.toButtonType() }
-	
-	/**
-	 * Shows given [MenuScene]. If [BoardGameScene] is currently displayed, it gets deactivated and blurred.
-	 *
-	 * @param scene menu scene to show.
-	 * @param fadeTime time to fade in, specified in seconds. Default: [Frontend.DEFAULT_FADE_TIME].
-	 */
-	fun showMenuScene(scene: MenuScene, fadeTime: Number = Frontend.DEFAULT_FADE_TIME) {
-		Frontend.showMenuScene(scene, fadeTime.toDouble())
-	}
-	
-	/**
-	 * Hides currently shown [MenuScene]. Activates [BoardGameScene] if present.
-	 *
-	 * @param fadeTime time to fade out, specified in seconds. Default: [Frontend.DEFAULT_FADE_TIME].
-	 */
-	fun hideMenuScene(fadeTime: Number = Frontend.DEFAULT_FADE_TIME) {
-		Frontend.hideMenuScene(fadeTime.toDouble())
-	}
-	
-	/**
-	 * Shows given [BoardGameScene].
-	 *
-	 * @param scene [BoardGameScene] to show.
-	 */
-	fun showGameScene(scene: BoardGameScene) {
-		Frontend.showGameScene(scene)
-	}
-	
-	/**
-	 * Sets [Alignment] of all [Scene]s in this [BoardGameApplication].
-	 */
-	fun setSceneAlignment(newAlignment: Alignment) {
-		setHorizontalSceneAlignment(newAlignment.horizontalAlignment)
-		setVerticalSceneAlignment(newAlignment.verticalAlignment)
-	}
-	
-	/**
-	 * Sets [HorizontalAlignment] of all [Scene]s in this [BoardGameApplication].
-	 */
-	fun setHorizontalSceneAlignment(newHorizontalAlignment: HorizontalAlignment) {
-		Frontend.setHorizontalSceneAlignment(newHorizontalAlignment)
-	}
-	
-	/**
-	 * Sets [VerticalAlignment] of all [Scene]s in this [BoardGameApplication].
-	 */
-	fun setVerticalSceneAlignment(newVerticalAlignment: VerticalAlignment) {
-		Frontend.setVerticalSceneAlignment(newVerticalAlignment)
-	}
-	
-	/**
-	 * Sets [ScaleMode] of all [Scene]s in this [BoardGameApplication].
-	 */
-	fun setScaleMode(newScaleMode: ScaleMode) {
-		Frontend.setScaleMode(newScaleMode)
-	}
+
+    /**
+     * [Frontend] instance.
+     */
+    private val frontend: Frontend = Frontend()
+
+    /**
+     * Window title displayed in the title bar.
+     */
+    var title: String
+        get() = Frontend.titleProperty.value
+        set(value) {
+            Frontend.titleProperty.value = value
+        }
+
+    /**
+     * Background [Visual] for the [BoardGameApplication]. [Visual] appears as bars window does not match [Scene] ratio.
+     * Do not mix up this [Property] with the [Scene] background [Visual] [Scene.background].
+     */
+    var background: Visual
+        get() = Frontend.backgroundProperty.value
+        set(value) {
+            Frontend.backgroundProperty.value = value
+        }
+
+    init {
+        title = windowTitle
+    }
+
+    /**
+     * Shows the given [FileDialog].
+     *
+     * @param dialog the [FileDialog] to be shown.
+     *
+     * @return chosen file(s) or [Optional.empty] if canceled.
+     */
+    fun showFileDialog(dialog: FileDialog): Optional<List<File>> = Frontend.showFileDialog(dialog)
+
+    /**
+     * Shows a dialog containing the given [message] and [buttons].
+     *
+     * @param alertType the [AlertType] of the alert. Affects the displayed icon.
+     * @param message message to be shown.
+     * @param buttons buttons to be shown.
+     *
+     * @return chosen button or [Optional.empty] if canceled.
+     */
+    fun showAlertDialog(alertType: AlertType, message: String, vararg buttons: ButtonType): Optional<ButtonType> =
+        Alert(
+            alertType.toAlertType(),
+            message,
+            *buttons.map { it.toFXButtonType() }.toTypedArray()
+        ).showAndWait().map { it.toButtonType() }
+
+    /**
+     * Shows given [MenuScene]. If [BoardGameScene] is currently displayed, it gets deactivated and blurred.
+     *
+     * @param scene menu scene to show.
+     * @param fadeTime time to fade in, specified in seconds. Default: [Frontend.DEFAULT_FADE_TIME].
+     */
+    fun showMenuScene(scene: MenuScene, fadeTime: Number = Frontend.DEFAULT_FADE_TIME) {
+        Frontend.showMenuScene(scene, fadeTime.toDouble())
+    }
+
+    /**
+     * Hides currently shown [MenuScene]. Activates [BoardGameScene] if present.
+     *
+     * @param fadeTime time to fade out, specified in seconds. Default: [Frontend.DEFAULT_FADE_TIME].
+     */
+    fun hideMenuScene(fadeTime: Number = Frontend.DEFAULT_FADE_TIME) {
+        Frontend.hideMenuScene(fadeTime.toDouble())
+    }
+
+    /**
+     * Shows given [BoardGameScene].
+     *
+     * @param scene [BoardGameScene] to show.
+     */
+    fun showGameScene(scene: BoardGameScene) {
+        Frontend.showGameScene(scene)
+    }
+
+    /**
+     * Sets [Alignment] of all [Scene]s in this [BoardGameApplication].
+     */
+    fun setSceneAlignment(newAlignment: Alignment) {
+        setHorizontalSceneAlignment(newAlignment.horizontalAlignment)
+        setVerticalSceneAlignment(newAlignment.verticalAlignment)
+    }
+
+    /**
+     * Sets [HorizontalAlignment] of all [Scene]s in this [BoardGameApplication].
+     */
+    fun setHorizontalSceneAlignment(newHorizontalAlignment: HorizontalAlignment) {
+        Frontend.setHorizontalSceneAlignment(newHorizontalAlignment)
+    }
+
+    /**
+     * Sets [VerticalAlignment] of all [Scene]s in this [BoardGameApplication].
+     */
+    fun setVerticalSceneAlignment(newVerticalAlignment: VerticalAlignment) {
+        Frontend.setVerticalSceneAlignment(newVerticalAlignment)
+    }
+
+    /**
+     * Sets [ScaleMode] of all [Scene]s in this [BoardGameApplication].
+     */
+    fun setScaleMode(newScaleMode: ScaleMode) {
+        Frontend.setScaleMode(newScaleMode)
+    }
 
 //	/**
 //	 * Sets this [BoardGameApplication] to fullscreen mode.
@@ -145,25 +146,25 @@ open class BoardGameApplication(windowTitle: String = "BoardGameWork Application
 //	fun setFullScreen(fullscreen: Boolean) {
 //		Frontend.setFullScreen(fullscreen)
 //	}
-	
-	/**
-	 * Manually refreshes currently displayed [Scene]s.
-	 */
-	fun repaint() {
-		Frontend.updateScene()
-	}
-	
-	/**
-	 * Shows the [BoardGameApplication].
-	 */
-	fun show() {
-		frontend.show()
-	}
 
-	/**
-	 * Returns the [show] function, thus closing the application window.
-	 */
-	fun exit() {
-		frontend.exit()
-	}
+    /**
+     * Manually refreshes currently displayed [Scene]s.
+     */
+    fun repaint() {
+        Frontend.updateScene()
+    }
+
+    /**
+     * Shows the [BoardGameApplication].
+     */
+    fun show() {
+        frontend.show()
+    }
+
+    /**
+     * Returns the [show] function, thus closing the application window.
+     */
+    fun exit() {
+        frontend.exit()
+    }
 }
