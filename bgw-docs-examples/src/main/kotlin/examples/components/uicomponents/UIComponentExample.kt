1b5bb4837bc5592212ca8212854f0aa53bf9ba0f
Fabian Klümpers <fabian.kluempers@udo.edu>
2021-08-10 11:32:35 +0200

    Add labels to UIComponents in UIComponentExample.kt.

--- bgw-docs-examples/src/main/kotlin/examples/components/uicomponents/UIComponentExample.kt
+++ bgw-docs-examples/src/main/kotlin/examples/components/uicomponents/UIComponentExample.kt
@@ -4,6 +4,7 @@ import tools.aqua.bgw.components.uicomponents.*
 import tools.aqua.bgw.core.Alignment
 import tools.aqua.bgw.core.BoardGameApplication
 import tools.aqua.bgw.core.MenuScene
+import tools.aqua.bgw.visual.ColorVisual
 import java.awt.Color
 
 fun main() {
@@ -13,19 +14,19 @@ fun main() {
 class UIComponentExample : BoardGameApplication("UIComponent Example") {
 	private val menuScene = MenuScene(width = 800)
 
-	private val outputLabel = Label(posX = 50, posY = 50, width = 300, text = "I am a label.")
+	private val outputLabel = Label(posX = 50, posY = 50, width = 300, text = "I am a Label.")
 
 	init {
 		menuScene.addComponents(outputLabel)
 
-		val button = Button(posX = 450, posY = 50, text = "I am a button.")
+		val button = Button(posX = 450, posY = 50, text = "I am a Button.")
 		button.onMouseClicked = {
 			outputLabel.text = "Someone pressed the Button!"
 		}
 		menuScene.addComponents(button)
 
 		val checkBox =
-			CheckBox(posX = 50, posY = 150, width = 300, text = "I am a CheckBox", alignment = Alignment.CENTER_LEFT)
+			CheckBox(posX = 50, posY = 150, width = 300, text = "I am a CheckBox.", alignment = Alignment.CENTER_LEFT)
 		checkBox.allowIndeterminate = true
 		checkBox.checked = true
 		checkBox.checkedProperty.addListener { _, newValue ->
@@ -43,7 +44,7 @@ class UIComponentExample : BoardGameApplication("UIComponent Example") {
 		}
 		menuScene.addComponents(colorPicker)
 
-		val comboBox = ComboBox<Double>(posX = 50, 300, width = 300, prompt = "I am a combo box.")
+		val comboBox = ComboBox<Double>(posX = 50, 350, width = 300, prompt = "Select an option! This is the prompt.")
 		comboBox.items = mutableListOf(0.0, 1.0, 2.0)
 		comboBox.formatFunction = {
 			"Option ${it.toInt()}"
@@ -51,9 +52,17 @@ class UIComponentExample : BoardGameApplication("UIComponent Example") {
 		comboBox.selectedItemProperty.addListener { _, newValue ->
 			outputLabel.text = "Combo box selection is : $newValue"
 		}
-		menuScene.addComponents(comboBox)
+		val comboBoxLabel = Label(
+			posX = comboBox.posX,
+			posY = comboBox.posY - 50,
+			width = comboBox.width,
+			height = comboBox.height,
+			alignment = Alignment.CENTER,
+			text = "This is a ComboBox"
+		)
+		menuScene.addComponents(comboBox, comboBoxLabel)
 
-		val progressBar = ProgressBar(posX = 450, posY = 300, width = 300, progress = 0.5, barColor = Color.GREEN)
+		val progressBar = ProgressBar(posX = 450, posY = 350, width = 300, progress = 0.5, barColor = Color.GREEN)
 		outputLabel.textProperty.addListener { _, _ ->
 			progressBar.progress = if (progressBar.progress + 0.05 > 1.0) 0.0 else progressBar.progress + 0.05
 		}
@@ -64,7 +73,14 @@ class UIComponentExample : BoardGameApplication("UIComponent Example") {
 				else -> progressBar.barColor = Color.BLUE
 			}
 		}
-		menuScene.addComponents(progressBar)
+		val progressBarLabel = Label(
+			posX = progressBar.posX,
+			posY = progressBar.posY - 50,
+			width = progressBar.width,
+			alignment = Alignment.CENTER,
+			text = "This is a ProgressBar."
+		)
+		menuScene.addComponents(progressBar, progressBarLabel)
 
 		val toggleGroup = ToggleGroup()
 		val radioButton = RadioButton(posX = 50, posY = 450, toggleGroup = toggleGroup)
@@ -73,7 +89,7 @@ class UIComponentExample : BoardGameApplication("UIComponent Example") {
 			posY = radioButton.posY,
 			width = 300 - radioButton.width,
 			height = radioButton.height,
-			text = "This is a radio button.",
+			text = "This is a RadioButton.",
 			alignment = Alignment.CENTER_LEFT
 		).apply { isWrapText = true }
 		radioButton.selectedProperty.addListener { _, newValue ->
@@ -85,7 +101,7 @@ class UIComponentExample : BoardGameApplication("UIComponent Example") {
 			posY = toggleButton.posY,
 			width = 300 - toggleButton.width,
 			height = toggleButton.height,
-			text = "This is a toggle button.",
+			text = "This is a ToggleButton.",
 			alignment = Alignment.CENTER_LEFT
 		).apply { isWrapText = true }
 		toggleButton.selectedProperty.addListener { _, newValue ->
@@ -94,15 +110,31 @@ class UIComponentExample : BoardGameApplication("UIComponent Example") {
 		}
 		menuScene.addComponents(radioButton, radioLabel, toggleButton, toggleLabel)
 
-		val textArea = TextArea(posX = 50, posY = 600, prompt = "I am a text area.")
+		val textArea = TextArea(posX = 50, posY = 600, prompt = "Type something! This is the prompt.")
 		textArea.textProperty.addListener { _, newValue ->
 			outputLabel.text = newValue
 		}
-		val textField = TextField(posX = 450, posY = 600, prompt = "I am a text field.")
+		val textAreaLabel = Label(
+			posX = textArea.posX,
+			posY = textArea.posY - 50,
+			width = textArea.width,
+			height = 50,
+			alignment = Alignment.CENTER,
+			text = "This is a TextArea."
+		)
+		val textField = TextField(posX = 450, posY = 600, width = 300, prompt = "Type something! This is the prompt.")
 		textField.textProperty.addListener { _, newValue ->
 			outputLabel.text = newValue
 		}
-		menuScene.addComponents(textArea, textField)
+		val textFieldLabel = Label(
+			posX = textField.posX,
+			posY = textField.posY - 50,
+			width = textField.width,
+			height = 50,
+			alignment = Alignment.CENTER,
+			text = "This is a TextField."
+		)
+		menuScene.addComponents(textArea, textAreaLabel, textField, textFieldLabel)
 
 		val listView = ListView<Int>(posX = 50, posY = 800, width = 300, height = 200)
 		listView.formatFunction = {
@@ -114,7 +146,7 @@ class UIComponentExample : BoardGameApplication("UIComponent Example") {
 			posY = listView.posY - 50,
 			width = listView.width,
 			height = 50,
-			text = "This is a list view.",
+			text = "This is a ListView.",
 			alignment = Alignment.CENTER
 		)
 		menuScene.addComponents(listView, listViewLabel)
@@ -129,7 +161,7 @@ class UIComponentExample : BoardGameApplication("UIComponent Example") {
 			posY = table.posY - 50,
 			width = table.width,
 			height = 50,
-			text = "This is a table view.",
+			text = "This is a TableView.",
 			alignment = Alignment.CENTER
 		)
 		menuScene.addComponents(table, tableLabel)
