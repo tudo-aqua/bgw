8b5676536ab6fcc1b3e06cca90e3e5f0bb5f0de1
Fabian Klümpers <fabian.kluempers@udo.edu>
2021-10-17 15:03:15 +0200

    Add KDoc to ParallelAnimation.kt and SequentialAnimation.kt

--- bgw-core/src/main/kotlin/tools/aqua/bgw/animation/ParallelAnimation.kt
+++ bgw-core/src/main/kotlin/tools/aqua/bgw/animation/ParallelAnimation.kt
@@ -1,3 +1,8 @@
 package tools.aqua.bgw.animation
 
+/**
+ * An Animation consisting of multiple Animations that are played in parallel, when this [Animation] is played.
+ *
+ * @param animations the [Animation]s that this [ParallelAnimation] should contain.
+ */
 data class ParallelAnimation(val animations: List<Animation>): Animation(animations.maxOf(Animation::duration))
