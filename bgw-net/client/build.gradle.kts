f79de3f338e885ddc7891fb0a305c095acabd94e
Fabian Klümpers <fabian.kluempers@udo.edu>
2021-10-17 17:42:14 +0200

    Remove application plugin from bgw-net submodules

--- bgw-net/client/build.gradle.kts
+++ bgw-net/client/build.gradle.kts
@@ -3,15 +3,11 @@ val kotlin_version: String by project
 val logback_version: String by project
 
 plugins {
-	application
 	kotlin("jvm")
 }
 
 group = "tools.aqua"
 version = "0.0.1"
-application {
-	mainClass.set("tools.aqua.bgw.net.client.ApplicationKt")
-}
 
 repositories {
 	mavenCentral()
--- bgw-net/server/build.gradle.kts
+++ bgw-net/server/build.gradle.kts
@@ -3,15 +3,11 @@ val kotlin_version: String by project
 val logback_version: String by project
 
 plugins {
-	application
 	kotlin("jvm")
 }
 
 group = "tools.aqua"
 version = rootProject.version
-application {
-	mainClass.set("tools.aqua.bgw.net.server.ApplicationKt")
-}
 
 repositories {
 	mavenCentral()
