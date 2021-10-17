ec3095af136ea04e3262c1e554cabb8223d4b75e
Fabian Klümpers <fabian.kluempers@udo.edu>
2021-10-17 15:07:53 +0200

    Fix KDoc style for parallel and sequential Animations

--- bgw-core/src/main/kotlin/tools/aqua/bgw/animation/SequentialAnimation.kt
+++ bgw-core/src/main/kotlin/tools/aqua/bgw/animation/SequentialAnimation.kt
@@ -3,7 +3,7 @@ package tools.aqua.bgw.animation
 /**
  * An [Animation] consisting of multiple [Animation]s that is played in sequence, when this [Animation] is played.
  *
- * @param animations the [Animation]s that this [SequentialAnimation] should contain.
+ * @param animations The [Animation]s that this [SequentialAnimation] should contain.
  * Lowest index is played first, highest index is played last.
  */
 data class SequentialAnimation(val animations: List<Animation>): Animation(animations.sumOf(Animation::duration))
