8a4caeed9056fef3b360ec21713bb576d697bc72
Fabian Klümpers <fabian.kluempers@udo.edu>
2021-10-18 14:11:38 +0200

    Add BoardGameClient and BoardGameSession classes as MVP

--- bgw-net/client/src/main/kotlin/tools/aqua/bgw/net/client/Application.kt
+++ bgw-net/client/src/main/kotlin/tools/aqua/bgw/net/client/Application.kt
@@ -4,46 +4,59 @@ import io.ktor.client.*
 import io.ktor.client.features.websocket.*
 import io.ktor.http.*
 import io.ktor.http.cio.websocket.*
+import io.ktor.utils.io.core.*
 import kotlinx.coroutines.*
-
-suspend fun DefaultClientWebSocketSession.outputMessages() {
-	try {
-		for (message in incoming) {
-			message as? Frame.Text ?: continue
-			val receivedText = message.readText()
-			println(receivedText)
-		}
-	} catch (e: Exception) {
-		println("Error while receiving: " + e.localizedMessage)
-	}
-}
-
-suspend fun DefaultClientWebSocketSession.inputMessages() {
-	while (true) {
-		val message = readLine() ?: ""
-		if (message.equals("exit", true)) return
-		try {
-			send(message)
-		} catch (e: Exception) {
-			println("Error while sending: " + e.localizedMessage)
-			return
-		}
-	}
-}
+import kotlin.coroutines.CoroutineContext
 
 fun main() {
-	val client = HttpClient {
-		install(WebSockets)
-	}
+	val client = BoardGameClient()
 	runBlocking {
-		client.webSocket(method = HttpMethod.Get, host = "127.0.0.1", port = 8080, path = "/chat") {
-			val messageOutputRoutine = launch { outputMessages() }
-			val userInputRoutine = launch { inputMessages() }
-
-			userInputRoutine.join() // Wait for completion; either "exit" or error
-			messageOutputRoutine.cancelAndJoin()
-		}
+		val session = client.startGame("127.0.0.1", 8080)
+		session.onMessageReceived = { println(it) }
+		session.send("Hello World!")
 	}
 	client.close()
 	println("Connection closed. Goodbye!")
+}
+
+class BoardGameClient() : CoroutineScope, Closeable {
+	private val client = HttpClient {
+		install(WebSockets)
+	}
+	override val coroutineContext: CoroutineContext
+		get() = client.coroutineContext
+	override fun close() {
+		client.close()
+	}
+	suspend fun startGame(host : String, port : Int) : BoardGameSession {
+		val session = BoardGameSession(client.webSocketSession(method = HttpMethod.Get, host = host, port = port, path = "/chat"))
+		session.setup()
+		return session
+	}
+}
+
+class BoardGameSession(private val defaultClientWebSocketSession: DefaultClientWebSocketSession) {
+	var onMessageReceived : ((String) -> Unit)? = null
+
+	suspend fun send(msg: String) {
+		defaultClientWebSocketSession.send(msg)
+	}
+
+	internal suspend fun setup() {
+		defaultClientWebSocketSession.launch {
+			defaultClientWebSocketSession.outputMessages()
+		}
+	}
+
+	private suspend fun DefaultClientWebSocketSession.outputMessages() {
+		try {
+			for (message in incoming) {
+				message as? Frame.Text ?: continue
+				val receivedText = message.readText()
+				onMessageReceived?.invoke(receivedText)
+			}
+		} catch (e: Exception) {
+			println("Error while receiving: " + e.localizedMessage)
+		}
+	}
 }
