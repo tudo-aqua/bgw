f86cd8003e466974ab45ccb89f0f0e36648f7d4a
Fabian Klümpers <fabian.kluempers@udo.edu>
2021-10-22 18:50:43 +0200

    Add secondary constructors for sequential and parallel animations. Improve KDoc for sequential and parallel animations.

--- bgw-core/src/main/kotlin/tools/aqua/bgw/animation/SequentialAnimation.kt
+++ bgw-core/src/main/kotlin/tools/aqua/bgw/animation/SequentialAnimation.kt
@@ -1,9 +1,21 @@
 package tools.aqua.bgw.animation
 
 /**
- * An [Animation] consisting of multiple [Animation]s that is played in sequence, when this [Animation] is played.
+ * An [Animation] consisting of multiple [Animation]s that is played in sequence,
+ * when this [SequentialAnimation] is played.
+ *
+ * @constructor creates a new [SequentialAnimation].
  *
  * @param animations The [Animation]s that this [SequentialAnimation] should contain.
  * Lowest index is played first, highest index is played last.
  */
-data class SequentialAnimation(val animations: List<Animation>): Animation(animations.sumOf(Animation::duration))
+data class SequentialAnimation(val animations: List<Animation>) : Animation(animations.sumOf(Animation::duration)) {
+
+	/**
+	 * creates a new [SequentialAnimation]
+	 * Additional constructor that enables the use of varargs for the animations.
+	 *
+	 * @param animation The [Animation]s that this [ParallelAnimation] should contain.
+	 */
+	constructor(vararg animation: Animation) : this(animation.toList())
+}
