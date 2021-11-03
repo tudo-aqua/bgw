beab0c8c7a8546609e92ac7ba31e7f887fc96031
Fabian Klümpers <fabian.kluempers@udo.edu>
2021-11-03 19:56:27 +0100

    Develop Message.kt further

--- bgw-net/common/src/main/kotlin/Message.kt
+++ bgw-net/common/src/main/kotlin/Message.kt
@@ -1,43 +1,83 @@
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.JsonElement
 
 //Message
 @Serializable
-sealed class Message()
+sealed class Message
 
+@Serializable
+data class GameMessage(val payload: JsonElement) : Message()
+
+//Request
 @Serializable
 sealed class Request : Message()
 
 @Serializable
-data class CreateGameRequest(val password: String, val numPlayers: Int) : Request()
+data class CreateGameRequest(val password: String) : Request()
 
 @Serializable
-data class JoinGameRequest(val id: Int, val password: String, val name: String, val playerIndex: Int) : Request()
+data class JoinGameRequest(val id: String, val password: String, val name: String) : Request()
 
 @Serializable
-data class GameMessageRequest(val payload: JsonElement) : Request()
-
+object DisconnectFromGameRequest : Request()
 
 //Response
 @Serializable
-sealed class Response() : Message()
+sealed class Response : Message()
 
 @Serializable
-data class CreateGameResponse(val state : State, val id: Int?) : Response()
+data class GameMessageResponse(val state: GameMessageState) : Response()
 
 @Serializable
-data class JoinGameResponse(val state: State) : Response()
+data class CreateGameResponse(val state: CreateGameState, val id: String?) : Response()
 
+@Serializable
+data class JoinGameResponse(val state: JoinGameState) : Response()
+
+@Serializable
+data class DisconnectFromGameResponse(val state: DisconnectFromGameState) : Response()
 
 //State
 @Serializable
-sealed class State
+enum class CreateGameState {
+	SUCCESS,
+	ALREADY_ASSOCIATED_WITH_GAME,
+	SERVER_ERROR
+}
 
 @Serializable
-object Success : State()
+enum class JoinGameState {
+	SUCCESS,
+	ALREADY_ASSOCIATED_WITH_GAME,
+	INVALID_ID,
+	INVALID_PASSWORD,
+	SERVER_ERROR
+}
 
 @Serializable
-class Failure(val reason: String) : State() //TODO replace String type of reason with something useful
+enum class DisconnectFromGameState {
+	SUCCESS,
+	NO_ASSOCIATED_GAME,
+	SERVER_ERROR
+}
+
+@Serializable
+enum class GameMessageState {
+	SUCCESS,
+	SCHEMA_NOT_FOUND,
+	INVALID_JSON,
+	SERVER_ERROR
+}
+
+//Notification
+@Serializable
+sealed class Notification : Message()
+
+@Serializable
+data class UserJoinedNotification(val name: String) : Notification()
+
+@Serializable
+data class UserDisconnectedNotification(val name: String) : Notification()
+
+@Serializable
+object GameEndedNotification : Notification()
