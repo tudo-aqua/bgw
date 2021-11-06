4928235c47fc5a30e6a07151a133a0d4c16cf9a2
Fabian Klümpers <fabian.kluempers@udo.edu>
2021-11-06 13:34:13 +0100

    Add some comments to JSONPlayground for demonstration purposes

--- bgw-net/client/src/main/kotlin/tools/aqua/bgw/net/client/JsonPlayground.kt
+++ bgw-net/client/src/main/kotlin/tools/aqua/bgw/net/client/JsonPlayground.kt
@@ -8,8 +8,10 @@ import org.everit.json.schema.loader.SchemaLoader
 import org.json.JSONObject
 
 
-//This is only a playground
+//this is only a playground
 
+//Classes to define Turns
+//these could be implemented by students based on the schema (maybe even generated)
 @Serializable
 data class Turn(val player: Player, val direction: Direction, val steps : Int)
 
@@ -23,8 +25,12 @@ enum class Direction() {
 }
 
 fun main() {
+	//encode a Turn object to JSON string
+	//this string would arrive at the server
 	val jsonString = Json.encodeToString<Turn>(Turn(player = Player("player1"), direction = Direction.LEFT, steps = 7))
 	println(jsonString)
+	//define the schema
+	//this schema would be stored on the server (developed by students)
 	val schemaVariable = "\"\$schema\": \"http://json-schema.org/draft-07/schema\"" //WARUM KANN MAN $ IN MULTILINE STRINGS NICHT ESCAPEN???
 	val schemaString = """
 		{
@@ -52,7 +58,10 @@ fun main() {
 			}
 		}
 	""".trimIndent()
+	//load the schema
+	//this happens on the server
 	val schema = SchemaLoader.load(JSONObject(schemaString))
+	//try validating the Turn object JSON string
 	try {
 		schema.validate(JSONObject(jsonString))
 	} catch (e : ValidationException) {
