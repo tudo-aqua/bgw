caad376d0f2bc962fa72307462b69a00495b08eb
Fabian Klümpers <fabian.kluempers@udo.edu>
2021-11-06 19:18:34 +0100

    Add first draft of BoardGameClient implementation

---
+++ bgw-net/client/src/main/kotlin/tools/aqua/bgw/net/client/BoardGameClient.kt
@@ -0,0 +1,94 @@
+package tools.aqua.bgw.net.client
+
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import tools.aqua.bgw.net.common.*
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.encodeToJsonElement
+import org.java_websocket.client.WebSocketClient
+import org.java_websocket.handshake.ServerHandshake
+import java.net.URI
+import kotlin.Exception
+
+class BoardGameClient(host: String, port: Int, endpoint: String) {
+	private val wsClient: WebSocketClient = MyWebSocketClient(URI.create("$host:$port/$endpoint"))
+
+	inner class MyWebSocketClient(uri: URI) : WebSocketClient(uri) {
+		override fun onOpen(handshakedata: ServerHandshake?) {
+			println("opened: $handshakedata")
+		}
+
+		override fun onMessage(message: String?) {
+			requireNotNull(message) {
+				//TODO exception handling
+			}
+			val bgwMessage : Message = Json.decodeFromString(message)
+			messageMapping(bgwMessage)
+		}
+
+		override fun onClose(code: Int, reason: String?, remote: Boolean) {
+			println("closed with code: $code and reason: $reason")
+		}
+
+		override fun onError(ex: Exception?) {
+			println(ex?.localizedMessage)
+		}
+
+		private fun messageMapping(message: Message) {
+			when(message) {
+				is Request -> throw Exception("Client received a request") //TODO error handling
+				is GameActionMessage -> onGameActionReceived?.invoke(message.payload)
+				is CreateGameResponse -> onCreateGameResponse?.invoke(message)
+				is GameActionResponse -> onGameActionResponse?.invoke(message)
+				is JoinGameResponse -> onJoinGameResponse?.invoke(message)
+				is LeaveGameResponse -> onLeaveGameResponse?.invoke(message)
+				is UserJoinedNotification -> onUserJoined?.invoke(message)
+				is UserDisconnectedNotification -> onUserLeft?.invoke(message)
+			}
+		}
+	}
+
+	fun connect() {
+		wsClient.connect()
+	}
+
+	fun disconnect() {
+		wsClient.closeBlocking()
+	}
+
+
+	fun createGame(gameID: String, sessionID : String, password : String) {
+		val message : Message = CreateGameMessage(gameID, sessionID, password)
+		wsClient.send(Json.encodeToString(message))
+	}
+
+	var onCreateGameResponse : ((CreateGameResponse) -> Unit)? = null
+
+
+	fun joinGame(sessionID: String, password: String, greeting : String) {
+		val message : Message = JoinGameMessage(sessionID, password, greeting)
+		wsClient.send(Json.encodeToString(message))
+	}
+
+	var onJoinGameResponse : ((JoinGameResponse) -> Unit)? = null
+
+
+	fun leaveGame() {
+		val message : Message = LeaveGameMessage
+		wsClient.send(Json.encodeToString(message))
+	}
+
+	var onLeaveGameResponse : ((LeaveGameResponse) -> Unit)? = null
+
+
+	var onGameActionResponse : ((GameActionResponse) -> Unit)? = null
+
+	var onGameActionReceived : ((payload: JsonElement) -> Unit)? = null
+
+
+	var onUserJoined : ((UserJoinedNotification) -> Unit)? = null
+
+	var onUserLeft : ((UserDisconnectedNotification) -> Unit)? = null
+
+}
