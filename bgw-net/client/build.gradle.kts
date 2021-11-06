390f72a5c72bb0ebe0dc5ada7ec1fc703c2055f3
Fabian Klümpers <fabian.kluempers@udo.edu>
2021-11-06 19:20:50 +0100

    Remove ktor code and dependency from client

--- bgw-net/client/build.gradle.kts
+++ bgw-net/client/build.gradle.kts
@@ -1,7 +1,3 @@
-val ktor_version: String by project
-val kotlin_version: String by project
-val logback_version: String by project
-
 plugins {
 	kotlin("jvm")
 	kotlin("plugin.serialization") version "1.5.31"
@@ -16,9 +12,9 @@ repositories {
 }
 
 dependencies {
-	implementation("io.ktor:ktor-client-websockets:$ktor_version")
-	implementation("io.ktor:ktor-client-cio:$ktor_version")
 	implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0")
+
+	implementation("org.java-websocket:Java-WebSocket:1.5.2")
 	implementation("com.github.everit-org.json-schema:org.everit.json.schema:1.14.0")
 	implementation(project(":bgw-net:common"))
 }
