972d616c5a2b43f5d451ead3c2698cb90ba2e2a0
Amin Bouzerda <amin.bouzerda@udo.edu>
2022-02-09 14:29:23 +0100

    Added JavaFX 17 dependency for mac-aarch64

--- bgw-examples/bgw-tetris-example/build.gradle.kts
+++ bgw-examples/bgw-tetris-example/build.gradle.kts
@@ -4,6 +4,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
 	kotlin("jvm")
+	application
 }
 
 group = "tools.aqua"
@@ -13,6 +14,10 @@ repositories {
 	mavenCentral()
 }
 
+application {
+	mainClass.set("tools.aqua.bgw.examples.tetris.main.MainKt")
+}
+
 dependencies {
 	implementation(kotlin("stdlib-jdk8"))
 	implementation(project(":bgw-ui"))
@@ -20,4 +25,15 @@ dependencies {
 
 tasks.withType<KotlinCompile> {
 	kotlinOptions.jvmTarget = "11"
+}
+
+tasks.withType<JavaExec> {
+    doFirst {
+        jvmArgs(
+			listOf(
+				"--module-path", classpath.asPath,
+				"--add-modules", "javafx.controls"
+			)
+		)
+    }
 }
--- bgw-ui/build.gradle.kts
+++ bgw-ui/build.gradle.kts
@@ -4,14 +4,14 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
 //	id("org.openjfx.javafxplugin") version "0.0.11"
-	id("org.jetbrains.dokka") version "1.4.32"
-	id("io.gitlab.arturbosch.detekt") version "1.18.0"
-	id("org.cadixdev.licenser") version "0.6.1"
-	id("com.dorongold.task-tree") version "2.1.0" // example usage: ./gradlew publish taskTree
-	`maven-publish`
-	kotlin("jvm")
+    id("org.jetbrains.dokka") version "1.4.32"
+    id("io.gitlab.arturbosch.detekt") version "1.18.0"
+    id("org.cadixdev.licenser") version "0.6.1"
+    id("com.dorongold.task-tree") version "2.1.0" // example usage: ./gradlew publish taskTree
+    `maven-publish`
+    kotlin("jvm")
 //	java
-	signing
+    signing
 }
 
 group = "tools.aqua"
@@ -20,55 +20,56 @@ var javaFxVersion: String = "17.0.2"
 extra["isReleaseVersion"] = !version.toString().endsWith("SNAPSHOT")
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
 //	implementation(kotlin("stdlib"))
-	implementation(kotlin("stdlib-jdk8"))
-	testImplementation(kotlin("test"))
-	
-	dokkaGfmPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:1.6.0")
+    implementation(kotlin("stdlib-jdk8"))
+    testImplementation(kotlin("test"))
 
-	/*
-	 * jfoenix - Applies styles to JavaFX controls.
-	 */
-	implementation(group="com.jfoenix", name="jfoenix", version="9.0.10")
-	
-	/*
-	 * javafx.base - Defines the base APIs for the JavaFX UI toolkit, including APIs for bindings, properties,
-	 * collections, and events.
-	 */
-	implementation(group="org.openjfx", name="javafx-base", version=javaFxVersion, classifier="win")
-	implementation(group="org.openjfx", name="javafx-base", version=javaFxVersion, classifier="mac")
-	implementation(group="org.openjfx", name="javafx-base", version=javaFxVersion, classifier="linux")
-	
-	/*
-	 * javafx.controls  - Defines the UI controls, charts, and skins that are available for the JavaFX UI toolkit.
-	 */
-	implementation(group="org.openjfx", name="javafx-controls", version=javaFxVersion, classifier="win")
-	implementation(group="org.openjfx", name="javafx-controls", version=javaFxVersion, classifier="mac")
-	implementation(group="org.openjfx", name="javafx-controls", version=javaFxVersion, classifier="linux")
-	
-	/*
-	 * javafx.graphics  - Defines the core scenegraph APIs for the JavaFX UI toolkit (such as layout containers,
-	 * application lifecycle, shapes, transformations, canvas, input, painting, image handling, and effects), as well
-	 * as APIs for animation, css, concurrency, geometry, printing, and windowing.
-	 */
-	implementation(group="org.openjfx", name="javafx-graphics", version=javaFxVersion, classifier="win")
-	implementation(group="org.openjfx", name="javafx-graphics", version=javaFxVersion, classifier="mac")
-	implementation(group="org.openjfx", name="javafx-graphics", version=javaFxVersion, classifier="linux")
-	
-	/* UNUSED MODULES:
-	 * javafx.fxml - Defines the FXML APIs for the JavaFX UI toolkit.
-	 *
-	 * javafx.media - Defines APIs for playback of media and audio content, as part of the JavaFX UI toolkit, including
-	                    MediaView and MediaPlayer.
-	 * javafx.swing - Defines APIs for the JavaFX / Swing interop support included with the JavaFX UI toolkit, including
-	                    SwingNode (for embedding Swing inside a JavaFX application) and JFXPanel (for embedding JavaFX
-	                    inside a Swing application).
-	 * javafx.web - Defines APIs for the WebView functionality contained within the JavaFX UI toolkit.
-	 */
+    dokkaGfmPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:1.6.0")
+
+    /*
+     * jfoenix - Applies styles to JavaFX controls.
+     */
+    implementation(group = "com.jfoenix", name = "jfoenix", version = "9.0.10")
+    /*
+     * javafx.base - Defines the base APIs for the JavaFX UI toolkit, including APIs for bindings, properties,
+     * collections, and events.
+     */
+    implementation(group = "org.openjfx", name = "javafx-base", version = javaFxVersion, classifier = "win")
+    implementation(group = "org.openjfx", name = "javafx-base", version = javaFxVersion, classifier = "mac")
+    implementation(group = "org.openjfx", name = "javafx-base", version = javaFxVersion, classifier = "mac-aarch64")
+    implementation(group = "org.openjfx", name = "javafx-base", version = javaFxVersion, classifier = "linux")
+    /*
+     * javafx.controls  - Defines the UI controls, charts, and skins that are available for the JavaFX UI toolkit.
+     */
+    implementation(group = "org.openjfx", name = "javafx-controls", version = javaFxVersion, classifier = "win")
+    implementation(group = "org.openjfx", name = "javafx-controls", version = javaFxVersion, classifier = "mac")
+    implementation(group = "org.openjfx", name = "javafx-controls", version = javaFxVersion, classifier = "mac-aarch64")
+    implementation(group = "org.openjfx", name = "javafx-controls", version = javaFxVersion, classifier = "linux")
+
+    /*
+     * javafx.graphics  - Defines the core scenegraph APIs for the JavaFX UI toolkit (such as layout containers,
+     * application lifecycle, shapes, transformations, canvas, input, painting, image handling, and effects), as well
+     * as APIs for animation, css, concurrency, geometry, printing, and windowing.
+     */
+    implementation(group = "org.openjfx", name = "javafx-graphics", version = javaFxVersion, classifier = "win")
+    implementation(group = "org.openjfx", name = "javafx-graphics", version = javaFxVersion, classifier = "mac")
+    implementation(group = "org.openjfx", name = "javafx-graphics", version = javaFxVersion, classifier = "mac-aarch64")
+    implementation(group = "org.openjfx", name = "javafx-graphics", version = javaFxVersion, classifier = "linux")
+
+    /* UNUSED MODULES:
+     * javafx.fxml - Defines the FXML APIs for the JavaFX UI toolkit.
+     *
+     * javafx.media - Defines APIs for playback of media and audio content, as part of the JavaFX UI toolkit, including
+                        MediaView and MediaPlayer.
+     * javafx.swing - Defines APIs for the JavaFX / Swing interop support included with the JavaFX UI toolkit, including
+                        SwingNode (for embedding Swing inside a JavaFX application) and JFXPanel (for embedding JavaFX
+                        inside a Swing application).
+     * javafx.web - Defines APIs for the WebView functionality contained within the JavaFX UI toolkit.
+     */
 }
 
 //javafx {
@@ -78,105 +79,105 @@ dependencies {
 //}
 
 tasks.test {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions.jvmTarget = "11"
+    kotlinOptions.jvmTarget = "11"
 }
 
 detekt {
-	source = files("src/main/kotlin/tools/aqua/bgw")
-	config = files("detekt-rules.yml")
+    source = files("src/main/kotlin/tools/aqua/bgw")
+    config = files("detekt-rules.yml")
 }
 
 val javadocJar: TaskProvider<Jar> by tasks.registering(Jar::class) {
-	archiveClassifier.set("javadoc")
-	from(tasks.dokkaJavadoc.flatMap { it.outputDirectory })
+    archiveClassifier.set("javadoc")
+    from(tasks.dokkaJavadoc.flatMap { it.outputDirectory })
 }
 
 java {
-	withSourcesJar()
-	withJavadocJar()
-	modularity.inferModulePath.set(true)
+    withSourcesJar()
+    withJavadocJar()
+    modularity.inferModulePath.set(true)
 }
 
 publishing {
-	publications {
-		create<MavenPublication>("maven") {
-			from(components["java"])
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
 
-			pom {
-				name.set("BoardGameWork Core Library")
+            pom {
+                name.set("BoardGameWork Core Library")
 
-				description.set(
-					"A framework for board game applications."
-				)
+                description.set(
+                    "A framework for board game applications."
+                )
 
-				url.set("https://github.com/tudo-aqua/bgw-core")
+                url.set("https://github.com/tudo-aqua/bgw-core")
 
-				licenses {
-					license {
-						name.set("Apache License, Version 2.0")
-						url.set("https://opensource.org/licenses/Apache-2.0")
-					}
-				}
+                licenses {
+                    license {
+                        name.set("Apache License, Version 2.0")
+                        url.set("https://opensource.org/licenses/Apache-2.0")
+                    }
+                }
 
-				developers {
-					developer {
-						name.set("Stefan Naujokat")
-						email.set("stefan.naujokat@tu-dortmund.de")
-					}
-					developer {
-						name.set("Till Schallau")
-						email.set("till.schallau@tu-dortmund.de")
-					}
-					developer {
-						name.set("Dominik Mäckel")
-						email.set("dominik.maeckel@tu-dortmund.de")
-					}
-					developer {
-						name.set("Fabian Klümpers")
-						email.set("fabian.kluempers@tu-dortmund.de")
-					}
-				}
+                developers {
+                    developer {
+                        name.set("Stefan Naujokat")
+                        email.set("stefan.naujokat@tu-dortmund.de")
+                    }
+                    developer {
+                        name.set("Till Schallau")
+                        email.set("till.schallau@tu-dortmund.de")
+                    }
+                    developer {
+                        name.set("Dominik Mäckel")
+                        email.set("dominik.maeckel@tu-dortmund.de")
+                    }
+                    developer {
+                        name.set("Fabian Klümpers")
+                        email.set("fabian.kluempers@tu-dortmund.de")
+                    }
+                }
 
-				scm {
-					connection.set("scm:git:git://github.com:tudo-aqua/bgw-core.git")
-					developerConnection.set("scm:git:ssh://git@github.com:tudo-aqua/bgw-core.git")
-					url.set("https://github.com/tudo-aqua/bgw-core/tree/main")
-				}
-			}
-		}
-	}
-	repositories {
-		maven {
-			name = "nexusOSS"
-			val releasesUrl = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
-			val snapshotsUrl = uri("https://oss.sonatype.org/content/repositories/snapshots/")
-			url = if (version.toString().endsWith("SNAPSHOT")) snapshotsUrl else releasesUrl
-			credentials {
-				username = properties["nexusUsername"] as? String
-				password = properties["nexusPassword"] as? String
-			}
-		}
-	}
+                scm {
+                    connection.set("scm:git:git://github.com:tudo-aqua/bgw-core.git")
+                    developerConnection.set("scm:git:ssh://git@github.com:tudo-aqua/bgw-core.git")
+                    url.set("https://github.com/tudo-aqua/bgw-core/tree/main")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            name = "nexusOSS"
+            val releasesUrl = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+            val snapshotsUrl = uri("https://oss.sonatype.org/content/repositories/snapshots/")
+            url = if (version.toString().endsWith("SNAPSHOT")) snapshotsUrl else releasesUrl
+            credentials {
+                username = properties["nexusUsername"] as? String
+                password = properties["nexusPassword"] as? String
+            }
+        }
+    }
 }
 
 signing {
-	useGpgCmd()
-	sign(publishing.publications["maven"])
+    useGpgCmd()
+    sign(publishing.publications["maven"])
 }
 
 tasks.withType<Sign>().configureEach {
-	onlyIf { project.extra["isReleaseVersion"] as Boolean }
+    onlyIf { project.extra["isReleaseVersion"] as Boolean }
 }
 
 tasks.named("publishMavenPublicationToNexusOSSRepository") {
-	dependsOn (tasks.checkLicenses)
+    dependsOn(tasks.checkLicenses)
 }
 
 license {
-	header(rootProject.file("LICENSE-HEADER.txt"))
+    header(rootProject.file("LICENSE-HEADER.txt"))
 }
 
