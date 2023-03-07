2f55cfb93554dc7141e4d50651359f7da572a496
Vadym.Kosin <Vadym.Kosin@tu-dortmund.de>
2023-03-07 16:55:24 +0100

    Added a test class for LayeredContainer

---
+++ bgw-gui/src/test/kotlin/tools/aqua/bgw/components/LayeredContainerTest.kt
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2023 The BoardGameWork Authors
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tools.aqua.bgw.components
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import tools.aqua.bgw.components.layoutviews.Pane
+import tools.aqua.bgw.components.uicomponents.Button
+import tools.aqua.bgw.core.DEFAULT_BUTTON_HEIGHT
+import tools.aqua.bgw.core.DEFAULT_BUTTON_WIDTH
+
+class LayeredContainerTest {
+  private var pane: Pane<ComponentView> =
+      Pane(width = DEFAULT_BUTTON_WIDTH, height = DEFAULT_BUTTON_HEIGHT)
+
+  private val layeredContainer: LayeredContainer<ComponentView>
+    get() = pane
+
+  private val button1 = Button()
+  private val button2 = Button()
+  private val button3 = Button()
+  private val button4 = Button()
+  private val button5 = Button()
+
+  @BeforeEach
+  fun setUp() {
+    pane.clear()
+    pane.addAll(button1, button2, button3, button4, button5)
+  }
+
+  @Test
+  fun toFrontTest() {
+    val expectedOrder = listOf<ComponentView>(button1, button3, button4, button5, button2)
+    layeredContainer.toFront(button2)
+    layeredContainer.children.forEachIndexed { i, child: ComponentView ->
+      assertEquals(expectedOrder[i], child)
+    }
+  }
+
+  @Test
+  fun toBackTest() {
+    val expectedOrder = listOf<ComponentView>(button4, button1, button2, button3, button5)
+    layeredContainer.toBack(button4)
+    layeredContainer.children.forEachIndexed { i, child: ComponentView ->
+      assertEquals(expectedOrder[i], child)
+    }
+  }
+
+  @Test
+  fun setZLayerTest() {
+    val expectedOrder =
+        listOf<Pair<ComponentView, Int>>(
+            button4 to -5, button5 to 0, button1 to 3, button2 to 10, button3 to 100)
+
+    println(expectedOrder)
+
+    for ((button, zIndex) in expectedOrder) {
+      layeredContainer.setZIndex(button, zIndex)
+    }
+
+    println(listOf(button1, button2, button3, button4, button5))
+    println(layeredContainer.children.toList())
+
+    layeredContainer.children.forEachIndexed { i, child: ComponentView ->
+      val (button, _) = expectedOrder[i]
+      assertEquals(button, child)
+    }
+  }
+}
