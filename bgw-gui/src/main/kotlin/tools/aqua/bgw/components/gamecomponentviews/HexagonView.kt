d09c8ead0d6204d6ec08193bcfb6670bb095b901
Amin Bouzerda <amin.bouzerda@tu-dortmund.de>
2023-03-13 17:31:33 +0100

    Added hexagon view component.

---
+++ bgw-gui/src/main/kotlin/tools/aqua/bgw/components/gamecomponentviews/HexagonView.kt
@@ -0,0 +1,11 @@
+package tools.aqua.bgw.components.gamecomponentviews
+
+import tools.aqua.bgw.visual.Visual
+
+class HexagonView(
+    posX: Number = 0,
+    posY: Number = 0,
+    width: Number = 100,
+    height: Number = 100,
+    visual: Visual
+) : GameComponentView(posX, posY, width, height, visual)
