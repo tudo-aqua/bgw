c1a18bb4a07f647e12959ae903606dc37a98271b
smilefx <SmileFX.LM@gmail.com>
2023-08-27 00:28:07 +0200

    Added example for Visual styles, filters and flipping

--- bgw-gui/src/jvmMain/kotlin/tools/aqua/bgw/main/Server.kt
+++ bgw-gui/src/jvmMain/kotlin/tools/aqua/bgw/main/Server.kt
@@ -8,9 +8,17 @@ import tools.aqua.bgw.components.uicomponents.Button
 import tools.aqua.bgw.components.uicomponents.Label
 import tools.aqua.bgw.core.BoardGameApplication
 import tools.aqua.bgw.core.BoardGameScene
+import tools.aqua.bgw.core.Color
 import tools.aqua.bgw.core.Scene
+import tools.aqua.bgw.style.BlurFilter
+import tools.aqua.bgw.style.BorderRadius
+import tools.aqua.bgw.style.Flip
+import tools.aqua.bgw.style.SaturationFilter
+import tools.aqua.bgw.util.Font
 import tools.aqua.bgw.visual.ColorVisual
+import tools.aqua.bgw.visual.CompoundVisual
 import tools.aqua.bgw.visual.ImageVisual
+import tools.aqua.bgw.visual.TextVisual
 import kotlin.random.Random
 
 
@@ -36,7 +44,23 @@ fun getRandomImageVisual(): ImageVisual {
 fun main() {
     val application = object : BoardGameApplication() {
         val scene = object : BoardGameScene(1920.0, 1080.0, ColorVisual.GREEN) {
-            val label = Label(visual = ColorVisual.RED, width = 200, height = 200, text = "Hello, SoPra!")
+            val label = Label(posX = 0, posY = 0, visual = CompoundVisual(
+                ImageVisual("https://cdn2.thecatapi.com/images/9qh.jpg").apply {
+                    flipped = Flip.HORIZONTAL
+                },
+                ColorVisual(Color.CYAN).apply {
+                    style.borderRadius = BorderRadius.FULL
+                    filters.blur = BlurFilter(4.0)
+                    filters.saturation = SaturationFilter.BLACK_WHITE
+                },
+                //ColorVisual(Color.GREEN).apply { style.borderRadius = BorderRadius(10,0,5,0) },
+                TextVisual("Hello World", font = Font(
+                    family = "Times New Roman",
+                    size = 20.0,
+                    color = Color.BLUE
+                )
+                )
+            ), width = 200, height = 200, text = "")
             val label2 = Label(posX = 200, posY = 200, visual = getRandomImageVisual(), width = 200, height = 200, text = "Hello, SoPra!")
 
 
