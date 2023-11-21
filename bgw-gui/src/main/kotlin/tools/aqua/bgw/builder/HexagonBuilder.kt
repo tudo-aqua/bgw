9080b4485dbc68877adc75c8386bfe8386ad6262
abouzerda <amin.bouzerda@udo.edu>
2023-11-21 18:41:19 +0100

    [bgw-gui] Added simple HexagonView CSS Styling.

--- bgw-gui/src/main/kotlin/tools/aqua/bgw/builder/HexagonBuilder.kt
+++ bgw-gui/src/main/kotlin/tools/aqua/bgw/builder/HexagonBuilder.kt
@@ -29,9 +29,12 @@ import javafx.scene.shape.StrokeLineJoin
 import javafx.scene.shape.StrokeType
 import kotlin.math.cos
 import kotlin.math.sin
+import kotlin.math.sqrt
 import tools.aqua.bgw.builder.FXConverters.toFXColor
 import tools.aqua.bgw.builder.FXConverters.toFXImage
 import tools.aqua.bgw.components.gamecomponentviews.HexagonView
+import tools.aqua.bgw.style.color
+import tools.aqua.bgw.style.pixel
 import tools.aqua.bgw.visual.*
 
 /**
@@ -55,49 +58,77 @@ object HexagonBuilder {
       root.children.clear()
       val component =
           when (nV) {
-            is TextVisual -> buildPolygon(points, nV)
-            is SingleLayerVisual -> buildPolygon(points, nV)
-            is CompoundVisual -> buildPolygon(points, nV)
+            is TextVisual -> buildPolygon(points, nV, hexagonView)
+            is SingleLayerVisual -> buildPolygon(points, nV, hexagonView)
+            is CompoundVisual -> buildPolygon(points, nV, hexagonView)
           }
       root.children.add(component)
     }
     return root
   }
 
-  private fun buildPolygon(points: DoubleArray, compoundVisual: CompoundVisual): Region =
+  private fun buildPolygon(
+      points: DoubleArray,
+      compoundVisual: CompoundVisual,
+      hexagonView: HexagonView
+  ): Region =
       StackPane(
               *compoundVisual.children
                   .map {
                     when (it) {
-                      is TextVisual -> buildPolygon(points, it)
-                      else -> buildPolygon(points, it)
+                      is TextVisual -> buildPolygon(points, it, hexagonView)
+                      else -> buildPolygon(points, it, hexagonView)
                     }
                   }
                   .toTypedArray())
           .apply { isPickOnBounds = false }
 
-  private fun buildPolygon(points: DoubleArray, visual: SingleLayerVisual): Region =
-      Pane(
-              Polygon(*points).apply {
-                val paint = buildPaint(visual)
-                fill = paint
-                visual.transparencyProperty.addListenerAndInvoke(visual.transparency) { _, nV ->
-                  opacity = nV
-                }
-                stroke = Color.BLACK
-                strokeType = StrokeType.INSIDE
-                // roundCorners(paint)
-              })
-          .apply { isPickOnBounds = false }
-
-  private fun buildPolygon(points: DoubleArray, visual: TextVisual): Region =
+  private fun buildPolygon(
+      points: DoubleArray,
+      visual: SingleLayerVisual,
+      hexagonView: HexagonView
+  ): Region {
+    return Pane(
+            Polygon(*points).apply {
+              val paint = buildPaint(visual)
+              fill = paint
+              visual.transparencyProperty.addListenerAndInvoke(visual.transparency) { _, nV ->
+                opacity = nV
+              }
+              visual.borderColorProperty.addListenerAndInvoke(visual.borderColor) { _, nV ->
+                stroke = nV?.color?.toFXColor() ?: Color.TRANSPARENT
+              }
+              strokeType = StrokeType.INSIDE
+              visual.borderWidthProperty.addListenerAndInvoke(visual.borderWidth) { _, nV ->
+                strokeWidth =
+                    if (nV != null && nV.pixel > 0) {
+                      nV.pixel.toDouble()
+                    } else 0.0
+              }
+              visual.borderRadiusProperty.addListenerAndInvoke(visual.borderRadius) { _, nV ->
+                clip =
+                    if (nV != null && nV.pixel > 0) {
+                      val size = hexagonView.size.toDouble() - nV.pixel.toDouble()
+                      val offsetX = hexagonView.widthProperty.value / 2 - sqrt(3.0) / 2 * size
+                      val offsetY = hexagonView.heightProperty.value / 2 - size
+                      Polygon(*generatePoints(size, offsetX, offsetY)).apply { roundCorners() }
+                    } else null
+              }
+            })
+        .apply { isPickOnBounds = false }
+  }
+  private fun buildPolygon(
+      points: DoubleArray,
+      visual: TextVisual,
+      hexagonView: HexagonView
+  ): Region =
       StackPane(
-              buildPolygon(points, visual as SingleLayerVisual),
+              buildPolygon(points, visual as SingleLayerVisual, hexagonView),
               VisualBuilder.buildVisual(visual).apply { isPickOnBounds = false })
           .apply { isPickOnBounds = false }
 
-  private fun Polygon.roundCorners(paint: Paint) {
-    stroke = paint
+  private fun Polygon.roundCorners() {
+    stroke = Color.BLACK
     strokeWidth = BORDER_WIDTH
     strokeType = StrokeType.CENTERED
     strokeLineJoin = StrokeLineJoin.ROUND
@@ -105,15 +136,19 @@ object HexagonBuilder {
     strokeMiterLimit = BORDER_WIDTH
   }
 
-  private fun generatePoints(size: Double): DoubleArray {
+  private fun generatePoints(
+      size: Double,
+      offsetX: Number = 0.0,
+      offsetY: Number = 0.0
+  ): DoubleArray {
     val points = mutableListOf<Double>()
     var angle = 90.0
     repeat(HEXAGON_SIDES) {
-      val x = size * cos(Math.toRadians(angle)) + size
+      val x = size * cos(Math.toRadians(angle)) + size / 2 * sqrt(3.0)
       val y = size * sin(Math.toRadians(angle)) + size
       angle += FULL_CIRCLE_DEGREES / HEXAGON_SIDES
-      points.add(x)
-      points.add(y)
+      points.add(x + offsetX.toDouble())
+      points.add(y + offsetY.toDouble())
     }
     return points.toDoubleArray()
   }
